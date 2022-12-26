b6c4d90755304075a0f9a25f8695840dd690021e
SANMATHI07 <115545050+SANMATHI07@users.noreply.github.com>
2022-12-26 23:22:46 +0530

    Update behave.sh

--- behave.sh
+++ behave.sh
@@ -2,42 +2,43 @@
 echo "installing behave"
 python_install()
 {
-    sudo apt install python3
-    if [ $? -ne 0 ]; then
-            echo "error while installing behave"
-    fi
-    echo "python installed successfully"
- }
- behave_install()
- {
-     sudo apt install python3-behave
-     if [ $? -ne 0 ]; then
-            echo "error while installing behave"
-     fi
-     echo "behave installed successfully"
- }
- cleanup_workspace()
- {
-      echo "removing old behave files"
-      sudo rm -r /var/lib/jenkins/workspace/testbehave/features
-      if [ $? -ne 0 ]; then
-              echo "error while removing behave files"
-      fi
-      echo "behave files removed successfully"
-  }
-  behave_steps()
-  {
-       echo "steps to run BDD"
-       sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
-       sudo cp -r ./test.feature /var/lib/jenkins/workspace/testbehave/features
-       sudo cp -r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
-       cd /var/lib/jenkins/workspace/testbehave
-       behave
-  }
-  python_install
-  behave_install
-  cleanup_workspace
-  behave_steps
+       sudo apt install python3 -y
+       if [ $? -ne 0 ]; then
+                echo "error while installing behave"
+       fi
+       echo "python installed successfully"
+}
+behave_install()
+{
+       sudo apt install python3-behave -y
+       if [ $? -ne 0 ]; then
+                echo "error while installing behave"
+       fi
+       echo "behave installed successfully"
+}
+cleanup_workspace()
+{
+       echo "removing old beahve files"
+       sudo rm -r /var/lib/jenkins/workspace/testbehave
+       if [ $? -ne 0 ]; then
+                echo "error while removing behave files"
+       fi
+       echo "behave files removed successfully"
+}
+behave_steps()
+{
+        echo "steps to run BDD"
+        sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
+        sudo cp -r ./test.feature /var/lib/jenkins/workspace/testbehave/features
+        sudo cp -r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
+        cd /var/lib/jenkins/workspace/testbehave
+        behave
+        
+}
+python_install
+behave_install
+cleanup_workspace
+behave_steps
   
               
      
